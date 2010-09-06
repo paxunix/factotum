f1ee2929935f9ba5992f17e63d912bd72a5ef049
Pax Unix <paxunix@gmail.com>
2010-09-06 13:41:45 -0700

    using new omnibox API
    - no longer triggering by hotkey
    - has a popup browser action now

--- scripts/hotkey.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('keyup', function (e) {
-    if (e.which == 32 && e.ctrlKey && !e.metaKey && !e.altKey)
-    {
-        chrome.extension.sendRequest({action: "open"});
-    }
-}, false);
