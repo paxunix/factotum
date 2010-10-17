321f0b70cf12702e90e4103f94b54aad0c55852d
Pax Unix <paxunix@gmail.com>
2010-10-17 10:57:34 -0700

    Initial support for registering an F-command.
    Registering currently does nothing but error-check the request.
    An in-extension request listener is supports testing by using the same
    listening function as the external request listener.

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,44 @@
-var omnibox = chrome.experimental.omnibox;
-
-var onInputStarted = function()
+// Function: listener
+//      Listen for requests from other extensions.
+//
+// Parameters:
+//      request - data request object.  Possible requests:
+//          register: {     - registers the sending extension as a handler
+//                            for the commands in commandList.
+//              commandList:  array of commands
+//              optionSpec:  option specification (see GetOpt.getOptions)
+//                  for the commands
+//              }
+function listener(request, sender, sendResponse)
 {
-};
+    var response = {
+        success: false,
+        error: "Unrecognized request."
+    };
+
+    if (request.register)
+    {
+        // Need to catch all exceptions so an error due to bad data from a
+        // request doesn't kill us.
+        try {
+
+        if (!jQuery.isArray(request.register.factotumCommands))
+        {
+            throw("request.register.factotumCommands must be an array.");
+        }
+
+        response = { success: true };
+
+        }   // try
+
+        catch (e)
+        {
+            response.error = e;
+        }
+    }
+
+    sendResponse(response);
+}  // listener
+
+chrome.extension.onRequestExternal.addListener(listener);
+chrome.extension.onRequest.addListener(listener);       // for testing
--- test/spec/spec-getopt.js
+++ test/spec/spec-getopt.js
@@ -469,3 +469,46 @@ describe("GetOpt.getOptions", function() {
     });
 
 }); // GetOpt.getOptions
+
+
+describe("Factotum", function() {
+
+    it("returns an error response if an unknown request is received", function() {
+        var response = { };
+
+        chrome.extension.sendRequest( { hello: "world" }, function(r) {
+            response = r;
+        });
+
+        waitsFor(function() {
+            return typeof(response.success) !== "undefined"
+        }, "Unrecognized request error.", 2000);
+
+        runs(function() {
+            expect(response).toEqual({
+                success: false,
+                error: "Unrecognized request."
+            })
+        });
+    });
+
+    it("returns an error response if request.register.factotumCommands is not an array", function() {
+        var response = { };
+
+        chrome.extension.sendRequest( { register: "bogus" }, function(r) {
+            response = r;
+        });
+
+        waitsFor(function() {
+            return typeof(response.success) !== "undefined"
+        }, "Unrecognized request error.", 2000);
+
+        runs(function() {
+            expect(response).toEqual({
+                success: false,
+                error: "request.register.factotumCommands must be an array."
+            })
+        });
+    });
+
+});    // Factotum
