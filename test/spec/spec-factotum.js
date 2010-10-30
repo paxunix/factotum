d0432013981a29c5f082e5424b6844773912a0c2
Pax Unix <paxunix@gmail.com>
2010-10-30 12:33:57 -0700

    added test for default empty optspec

--- test/spec/spec-factotum.js
+++ test/spec/spec-factotum.js
@@ -67,10 +67,12 @@ describe("Factotum", function() {
 
     it("saves each registered command name and optspec", function() {
         var response = { };
+        var optspec = { "a": { type: "boolean" } };
 
         chrome.extension.sendRequest({
             register: {
-                factotumCommands: [ "test", "testing" ]
+                factotumCommands: [ "test", "testing" ],
+                optionSpec: optspec,
                 }
             },
             function(r) {
@@ -88,13 +90,38 @@ describe("Factotum", function() {
             });
             expect(chrome.extension.getBackgroundPage().
                 Factotum.commands.test).
-                    toEqual({ optspec: {}, extensionId: extensionId });
+                    toEqual({ optspec: optspec, extensionId: extensionId });
             expect(chrome.extension.getBackgroundPage().
                 Factotum.commands.testing).
+                    toEqual({ optspec: optspec, extensionId: extensionId });
+        });
+    });
+
+    it("request.register.optionSpec can be missing and an empty optspec will be used", function() {
+        var response = { };
+
+        chrome.extension.sendRequest({
+            register: {
+                factotumCommands: [ "test2" ]
+                }
+            },
+            function(r) {
+                response = r;
+            }
+        );
+
+        waitsFor(function() {
+            return typeof(response.success) !== "undefined"
+        }, "Unrecognized request error.", 2000);
+
+        runs(function() {
+            expect(response).toEqual({
+                success: true
+            });
+            expect(chrome.extension.getBackgroundPage().
+                Factotum.commands.test2).
                     toEqual({ optspec: {}, extensionId: extensionId });
         });
     });
-    // XXX:request.register.optionSpec can be missing and an empty optspec will
-    // be used
 
 });    // Factotum
