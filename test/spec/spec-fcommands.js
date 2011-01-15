41f774d38fa1fb0fa37fd841c724bd2a2a0f7715
Pax Unix <paxunix@gmail.com>
2011-01-14 22:14:33 -0800

    fix typo

--- test/spec/spec-fcommands.js
+++ test/spec/spec-fcommands.js
@@ -1,6 +1,6 @@
 describe("Fcommands", function() {
 
-    it("has a get() method that throws null if the name has no value",
+    it("has a get() method that returns null if the name has no value",
         function() {
             expect(
                 Fcommands.get("")
