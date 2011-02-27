726541d563c4aaff2b1da461e707edc9efb2ff1c
Pax Unix <paxunix@nightrain.(none)>
2011-02-26 18:20:37 -0800

    Register internal commands.
    Like a stub for "help".

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,19 @@
 var Factotum = {};
 
 
+Factotum.registerInternalCommands = function()
+{
+    Fcommands.set({
+        names: [ "help" ],
+        guid: "c274b610-4215-11e0-9207-0800200c9a66",
+        description: "Factotum Help",
+        execute: function(cmdobj) { // XXX
+            console.log("Factotum help. Args:", cmdobj);
+        }
+    });
+};  // Factotum.registerInternalCommands
+
+
 // Listener for Omnibox input.
 Factotum.onOmniboxInputEntered = function(text)
 {
@@ -10,3 +23,5 @@ Factotum.onOmniboxInputEntered = function(text)
 
 // Register Omnibox listener.
 chrome.omnibox.onInputEntered.addListener(Factotum.onOmniboxInputEntered);
+
+Factotum.registerInternalCommands();
