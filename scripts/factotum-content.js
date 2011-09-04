fbed3722cc4658506cb054ef50f7d4e5486a3960
Pax Unix <paxunix@gmail.com>
2011-09-03 22:23:04 -0700

    Use a fixed content script for the wrapper.
    - give it a name including the extension's name so it's easy to find when
      debugging the content script).
    
    - This simplifies the execution of the user code because the extension no
      longer has to keep track of whether or not the wrapper has been injected
      into the user page yet.  It does mean the wrapper loads in every page,
      even if it will not be used.  The wrapper is not much code, though, so the
      impact from that should be minimal.
    
    - This also preserves the request/response behaviour for executing user code
      and returning a result to the extension.

--- scripts/factotum-content.js
+++ scripts/factotum-content.js
@@ -1,12 +1,13 @@
-// This code is dynamically injected into the current page the first time an
-// Fcommand is invoked.
-
 // Define a connection listener that executes Fcommand code passed from
 // Factotum.  If an exception occurs while executing the Fcommand, the error
 // message is returned to Factotum.
 // Variables prefixed with '_' are intended for internal use only.
 // XXX: using some tricks to remove them from the user code's scope would be
 // fancier and better.
+// XXX:  this code should be responsible for loading prerequisite scripts and
+// CSS, otherwise Factotum has to keep track of whether or not it has been done
+// for this page already.
+// XXX:  should restrict requests to only be executed if sent by Factotum.
 function factotumListener(_request, _sender, _responseFunc)
 {
     var cmdlineObj = _request.cmdlineObj;
