e2696f5748e908de1eee16c23b7a07014fb5e69f
Pax Unix <paxunix@gmail.com>
2011-09-08 22:49:34 -0700

    New FileSystem class.
    - will be used for Fcommand storage
    - implementation and tests for basic write() operation

---
+++ test/spec/spec-filesystem.js
@@ -0,0 +1,82 @@
+describe("FileSystem.write", function() {
+
+
+    it("calls the error function if a problem occurs during a filesystem operation", function() {
+        var msg = "";
+        var onError = function(e) {
+            msg = e;
+        };
+        var onSuccess = jasmine.createSpy();
+        
+        var fs = new FileSystem(1024, onError);
+        fs.writeFile(undefined, "bogusdata", onSuccess);
+
+        waitsFor(function() { return msg !== ""; }, "writeFile", 2000);
+
+        runs(function() {
+            expect(msg).toNotEqual("");
+            expect(onSuccess).not.toHaveBeenCalled();
+        });
+    });
+
+
+    it("calls the success function after successfully writing a file", function() {
+        var onError = jasmine.createSpy();
+        var success = false;
+        var obj = {
+            onSuccess: function() {
+                success = true;
+            }
+        };
+
+        spyOn(obj, "onSuccess").andCallThrough();
+        
+        var fs = new FileSystem(1024, onError);
+        fs.writeFile("testfile", "testdata", obj.onSuccess);
+
+        waitsFor(function() { return success; }, "writeFile", 2000);
+
+        runs(function() {
+            expect(success).toBe(true);
+            expect(obj.onSuccess).toHaveBeenCalled();
+        });
+    });
+
+    xit("writing to file of same name overwrites contents");
+
+    xit("writes a string of data to a filename", function() {
+        var filename = "test";
+        var data = "data";
+        var done = false;
+
+        var fs = new FileSystem(1024, function onError() {
+            throw "Failed FileSystem operation.";
+        });
+
+        fs.writeFile(filename, data, function onSuccess() {
+            done = true;
+        });
+
+        waitsFor(function() { return done; }, "file write failed", 5000);
+
+        // XXX: WTF to expect???
+        expect(suggestion.content).toEqual(name + " " + argv.join(" "));
+    });
+
+
+    xit("reads a string of data from a filename", function() {
+        var filename = "test";
+        var done = false;
+
+        var fs = new FileSystem(filename, data, function() {
+            done = true;
+        });
+
+        waitsFor(function() { return done; }, "file write failed", 5000);
+
+        // XXX: WTF to expect???
+        expect(suggestion.content).toEqual(name + " " + argv.join(" "));
+    });
+
+
+}); // FileSystem.write
