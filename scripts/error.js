64e2114650b4320937f284b4e6971fe3af159caa
Pax Unix <paxunix@gmail.com>
2011-09-30 21:42:32 -0700

    Add InvalidData exception class.

--- scripts/error.js
+++ scripts/error.js
@@ -31,3 +31,19 @@ function MissingPropertyError(message)
 
 MissingPropertyError.prototype = new FcommandError();
 MissingPropertyError.prototype.name = "MissingPropertyError";
+
+
+/** @class Fcommand "invalid data" error.
+ *
+ * @constructor
+ * @param {String} message Error message.
+ */
+function InvalidData(message)
+{
+    FcommandError.apply(this, arguments);
+
+    this.message = message;
+}   // InvalidData
+
+InvalidData.prototype = new FcommandError();
+InvalidData.prototype.name = "InvalidData";
--- test/spec/spec-error.js
+++ test/spec/spec-error.js
@@ -60,3 +60,16 @@ describe("MissingPropertyError", function() {
         expect(err.message).toEqual(msg);
     });
 }); // MissingPropertyError
+
+
+describe("InvalidData", function() {
+    it("constructs an InvalidData object with info about the exception", function() {
+        var msg = "error info";
+        var err = new InvalidData(msg);
+
+        expect(err instanceof Error).toBe(true);
+        expect(err instanceof FcommandError).toBe(true);
+        expect(err instanceof InvalidData).toBe(true);
+        expect(err.message).toEqual(msg);
+    });
+}); // InvalidData
