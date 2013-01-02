40917a151ca2c5c467aa5f16be82d57c37ffc305
Pax Unix <paxunix@gmail.com>
2013-01-01 18:01:01 -0800

    Update manifest and cleanup

--- test/spec/spec-FcommandManager.js
+++ test/spec/spec-FcommandManager.js
@@ -15,28 +15,3 @@ describe("FcommandManager", function() {
 
 
 }); // FcommandManager
-
-
-describe("FcommandManager.loadCommands", function() {
-
-    beforeEach(function() {
-        this.addMatchers({ toThrowInstanceOf: toThrowInstanceOf });
-    });
-
-    it("calls success callback when all Fcommands have been loaded.", function() {
-        var mgr = new FcommandManager();
-        var onSuccess = jasmine.createSpy();
-        var onError = jasmine.createSpy();
-
-        expect(function() {
-            mgr.loadCommands(onSuccess, onError);
-        }).not.toThrow();
-
-        waitsFor(function() { return onSuccess.wasCalled },
-            "load to succeed", 2000);
-
-        expect(onError).not.toHaveBeenCalled();
-    });
-
-
-}); // FcommandManager.loadCommands
