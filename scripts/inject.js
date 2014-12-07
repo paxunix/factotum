d9822792b55a87e8a87bd027eea0fc77d1a126f3
Pax Unix <paxunix@gmail.com>
2014-12-06 20:33:19 -0800

    Remove unneeded window. prefix

--- scripts/inject.js
+++ scripts/inject.js
@@ -10,7 +10,7 @@ window.Factotum = { };
  * Available to be called from with page-context Fcommand code.  It returns
  * the minimist options hash of the parsed Fcommand command line.
  */
-window.Factotum.getParameters = function (guid)
+Factotum.getParameters = function (guid)
 {
     var linkEl = document.querySelector("head link#fcommand-" +
             guid + "[rel=import]")
@@ -19,7 +19,7 @@ window.Factotum.getParameters = function (guid)
 }   // Factotum.getParameters
 
 
-window.Factotum.getInternalOptions = function (guid)
+Factotum.getInternalOptions = function (guid)
 {
     var linkEl = document.querySelector("head link#fcommand-" +
             guid + "[rel=import]")
@@ -32,7 +32,7 @@ window.Factotum.getInternalOptions = function (guid)
  * Return the ID of the currently running Fcommand (when called from
  * within the Fcommand's scripts running during import).
  */
-window.Factotum.getFcommandId = function ()
+Factotum.getFcommandId = function ()
 {
     var fcommandDoc = document.currentScript.ownerDocument;
     return fcommandDoc.querySelector("head meta[name=guid]").content;
@@ -44,12 +44,12 @@ window.Factotum.getFcommandId = function ()
  * code.
  * XXX: document fcommandFunc params
  */
-window.Factotum.runCommand = function (fcommandFunc)
+Factotum.runCommand = function (fcommandFunc)
 {
-    var fcommandId = window.Factotum.getFcommandId();
+    var fcommandId = Factotum.getFcommandId();
     var importDoc = document.currentScript.ownerDocument;
-    var parameters = window.Factotum.getParameters(fcommandId);
-    var internalOptions = window.Factotum.getInternalOptions(fcommandId);
+    var parameters = Factotum.getParameters(fcommandId);
+    var internalOptions = Factotum.getInternalOptions(fcommandId);
 
     var p = new Promise(function (resolve, reject) {
         if (internalOptions.debug)
@@ -65,7 +65,7 @@ window.Factotum.runCommand = function (fcommandFunc)
             data: bgData,
             internalOptions: internalOptions
         };
-        window.postMessage(data, "*");
+        postMessage(data, "*");
     }).catch(function (error) {
         // If a thrown Error, its details will not be preserved when passed
         // to the background context, so pull out the stack and use it
@@ -75,7 +75,7 @@ window.Factotum.runCommand = function (fcommandFunc)
             error: (error instanceof Error) ?  error.stack : error,
         };
 
-        window.postMessage(response, "*");
+        postMessage(response, "*");
 
         if (error instanceof Error)
             throw error;
