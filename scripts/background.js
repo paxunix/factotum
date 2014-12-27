4b4a20258a902aa2876fc835d34e6c5fcb3957ba
Pax Unix <paxunix@gmail.com>
2014-12-27 12:10:21 -0800

    Load test commands at startup
    - now that FcommandManager exists, don't store commands in a global cache

--- scripts/background.js
+++ scripts/background.js
@@ -3,15 +3,23 @@
 var FactotumBg = require("./FactotumBg.js");
 var FcommandManager = require("./FcommandManager.js");
 
-// XXX: during testing, to create an Fcommand on-the-fly
-window.Fcommand = require("./Fcommand.js");
+window.fcommandManager = new FcommandManager();
+
+// Register Omnibox listeners.
+chrome.omnibox.onInputEntered.addListener(FactotumBg.onOmniboxInputEntered);
+chrome.omnibox.onInputChanged.addListener(FactotumBg.onOmniboxInputChanged);
+
+// Listen for messages from content script
+chrome.runtime.onMessage.addListener(FactotumBg.responseHandler);
 
 if (1) {   // XXX:  this is for testing only.  Preload Fcommands so we can invoke them.
-window.XXXcommandCache = { };      // for testing only
+
+// XXX: during testing, to create an Fcommand on-the-fly in devtools
+window.Fcommand = require("./Fcommand.js");
 
 var Util = require("./Util.js");
-var Fcommand = require("./Fcommand.js");
 
+// Ensure some test Fcommands are always present
 var fetchThese = [
     Util.fetchDocument(chrome.runtime.getURL("example/load-jquery.html")),
     Util.fetchDocument(chrome.runtime.getURL("example/bgtest.html")),
@@ -19,10 +27,13 @@ var fetchThese = [
 
 fetchThese.forEach(function (p) {
     p.then(function resolvedWith(event) {
-        var fcommand = new Fcommand(event.target.responseText, navigator.language);
-        window.XXXcommandCache[fcommand.extractedData.guid] = fcommand;
-    }).
-    catch(function rejectedWith(data) {
+            var fcommand = new Fcommand(event.target.responseText, navigator.language);
+            return fcommandManager.save(fcommand);
+        })
+    .then(function (res) {
+            console.log("Test fcommand (" + res + ") loaded");
+        })
+    .catch(function rejectedWith(data) {
         console.log("Fcommand load failure:", data);
         chrome.notifications.create(
             "",
@@ -43,13 +54,4 @@ fetchThese.forEach(function (p) {
     });
 });
 
-}
-
-window.fcommandManager = new FcommandManager();
-
-// Register Omnibox listeners.
-chrome.omnibox.onInputEntered.addListener(FactotumBg.onOmniboxInputEntered);
-chrome.omnibox.onInputChanged.addListener(FactotumBg.onOmniboxInputChanged);
-
-// Listen for messages from content script
-chrome.runtime.onMessage.addListener(FactotumBg.responseHandler);
+}   // test code only
