f5265b725041dc96a1eaf629f22e2e02c491208a
Pax Unix <paxunix@gmail.com>
2017-01-20 21:37:50 -0800

    Fix fetching Fcommand URL
    - method now correctly returns a Promise
    - save the Fcommand after retrieval

--- scripts/FcommandManager.js
+++ scripts/FcommandManager.js
@@ -196,8 +196,9 @@ getAll()
  */
 fetchFcommandUrl(url)
 {
-    let p_getFcommand = Util.fetchDocument(url)
+    return Util.fetchDocument(url)
         .then(bodyText => new Fcommand(bodyText, navigator.language))
+        .then(fcommand => this.save(fcommand))
         .catch(error => {
             this.getErrorManager().save(error, `Fcommand fetch failure (${url})`)
         });
