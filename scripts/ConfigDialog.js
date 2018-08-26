c8d6ad31e2a4d164de08259cbe48f0d7efcd7ed4
Pax Unix <paxunix@gmail.com>
2018-08-25 20:58:08 -0700

    Move example code into doc comment

--- scripts/ConfigDialog.js
+++ scripts/ConfigDialog.js
@@ -1,5 +1,59 @@
-"use strict";
+//XXX:"use strict";
 
+
+/*
+
+// Simple class to popup a modal HTML5 dialog with configurable options.
+// Supports radio button groups, boolean checkboxes, and single-line text
+// input.
+
+d = new ConfigDialog({
+    title: "The Dialog Title",      // title is optional
+    sections: [ // rendering data
+        {
+            display: "Section 1",   // section display names are optional
+            options: [
+                { display: "Option 1", key: "opt1" },
+                { display: "Opt 2", key: "opt2" },
+            ]
+        },
+        {
+            display: "Sec 2",
+            options: [
+                { display: "Opt 2.1", key: "opt2.1" }
+            ]
+        },
+        {
+            display: "Radio Section",
+            options: [
+                // If an option has a value, it will be rendered as a radio
+                // button.  You will want the key for each radio button to
+                // be the same for that given grouping (which does not
+                // strictly have to be contained within a single section).
+                { display: "Rad 1", key: "optradio", value: "r1" },
+                { display: "Rad 2", key: "optradio", value: "r2" },
+                { display: "Rad 3", key: "optradio", value: "r3" },
+            ]
+        },
+    ]},
+    {   // initial data
+        opt1: true,
+        opt2: "some text",
+        "opt2.1": false,
+        "optradio": "r2",
+    },
+    document);
+
+d.show()
+    .then(data => { console.log("d data: ", data); })
+    .catch(() => { console.log("d cancelled/aborted"); });
+
+d2 = new ConfigDialog({ title: "Some Title", sections: [ { options: [ { display: "Thing 1", key: "thingval" } ] } ] }, {}, document);
+
+d2.show()
+    .then(data => { console.log("d2 data: ", data); })
+    .catch();
+*/
 
 const OKAY_TEXT = "Ok";
 const CANCEL_TEXT = "Cancel";
@@ -267,41 +321,3 @@ dialog.${STYLE_ID}::backdrop {
         this.dialog.innerHTML = this.getMarkup(newState);
     }
 };
-
-
-d = new ConfigDialog({
-    title: "The Dialog Title",
-    sections: [ // rendering data
-        {
-            display: "Section 1",
-            options: [
-                { display: "Option 1", key: "opt1" },
-                { display: "Opt 2", key: "opt2" },
-            ]
-        },
-        {
-            display: "Sec 2",
-            options: [
-                { display: "Opt 2.1", key: "opt2.1" }
-            ]
-        },
-        {
-            display: "Radio Section",
-            options: [
-                // If an option has a value, it will be rendered as a radio
-                // button.  You will want the key for each radio button to
-                // be the same for that given grouping (which does not
-                // strictly have to be contained within a single section).
-                { display: "Rad 1", key: "optradio", value: "r1" },
-                { display: "Rad 2", key: "optradio", value: "r2" },
-                { display: "Rad 3", key: "optradio", value: "r3" },
-            ]
-        },
-    ]},
-    {   // initial data
-        opt1: true,
-        opt2: "some text",
-        "opt2.1": false,
-        "optradio": "r2",
-    },
-    document);
